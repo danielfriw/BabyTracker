9365de90ef79c8c1fada2e1fd00f5cb5f38327ec
danielfriw <danielfri@wix.com>
2024-02-26 10:28:16 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import render_template, redirect, url_for, session
+from flask_login import login_required, current_user
+from main.blueprints.baby_blueprint.baby_blueprint import baby_blueprint
+from main.blueprints.auth_blueprint.auth_blueprint import auth_blueprint
+from main.blueprints.events_blueprint.events_blueprint import events_blueprint
+from main.blueprints.graphs_blueprint.graphs_blueprint import graphs_blueprint
+from main.blueprints.api_baby_blueprint.api_baby_blueprint import api_baby_blueprint
+from main import app, db
+from main.blueprints.baby_blueprint.models import Baby
+from main.blueprints.events_blueprint.models import Event
+from main.utils.baby_context_processor_for_navbar_dropdown_list import baby_context_processor_for_navbar_dropdown_list
+
+# Create tables (initialize the database)
+with app.app_context():
+    db.create_all()
+
+# Context processors: these run before the template is rendered
+# We use it here to render the users babies in the navbar dropdown list
+app.context_processor(baby_context_processor_for_navbar_dropdown_list)
+
+
+app.register_blueprint(baby_blueprint, url_prefix='/baby')
+app.register_blueprint(auth_blueprint, url_prefix='/auth')
+app.register_blueprint(events_blueprint, url_prefix='/events')
+app.register_blueprint(graphs_blueprint, url_prefix='/graphs')
+app.register_blueprint(api_baby_blueprint, url_prefix='/api/baby')
+
+
+@app.route('/')
+@login_required
+def index():
+    if session.get('baby_name') is None:
+        babies = Baby.query.filter_by(user_id=current_user.id).all()
+        if len(babies) > 0:
+            session['baby_name'] = babies[0].name
+            session["baby_gender"] = babies[0].gender
+            session['baby_id'] = babies[0].id
+        else:
+            return redirect(url_for('baby.add_baby'))
+
+    user_id = current_user.id
+    events = Event.query.filter_by(user_id=user_id).filter_by(baby_name=session['baby_name']).order_by(
+        Event.created_at.desc()).all()
+    return render_template('index.html', events=events)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ test_api.py
@@ -0,0 +1,37 @@
+import requests
+import json
+
+
+def make_api_get_call(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        print("API Response:")
+        print(response.json())
+    else:
+        print(f"Error: {response.status_code}, {response.text}")
+
+
+def make_api_post_call(url, data=None, headers=None):
+    response = requests.post(url, data=json.dumps(data), headers=headers)
+    if response.status_code == 200:
+        print("API Response:")
+        print(response.json())
+    else:
+        print(f"Error: {response.status_code}, {response.text}")
+
+
+def make_api_delete_call(url):
+    response = requests.delete(url)
+    if response.status_code == 200:
+        print("API Response:")
+        print(response.json())
+    else:
+        print(f"Error: {response.status_code}, {response.text}")
+
+
+if __name__ == "__main__":
+    make_api_get_call("http://127.0.0.1:5000/api/baby/1/Hadar")
+    # make_api_post_call("http://127.0.0.1:5000/api/baby/1/Sharon",
+    #                     data={"gender": "f", "dob": "2022-06-01"},
+    #                     headers={"Content-Type": "application/json"})
+    # make_api_delete_call("http://127.0.0.1:5000/api/baby/1/Shlomit")

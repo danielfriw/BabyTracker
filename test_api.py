12f8ea40f69a4f1827236789b1a8305fdf673f49
danielfriw <danielfri@wix.com>
2024-03-16 23:53:46 +0200

    refactored the percentile calc to be an API

--- test_api.py
+++ test_api.py
@@ -29,9 +29,12 @@ def make_api_delete_call(url):
         print(f"Error: {response.status_code}, {response.text}")
 
 
+
 if __name__ == "__main__":
     # make_api_get_call("http://127.0.0.1:5000/api/baby/1/Hadar")
-    make_api_post_call("http://127.0.0.1:5000/api/baby/1/Sharon",
-                        data={"gender": "f", "dob": "2022-06-01"},
-                        headers={"Content-Type": "application/json"})
+    # make_api_post_call("http://127.0.0.1:5000/api/baby/1/Sharon",
+    #                     data={"gender": "f", "dob": "2022-06-01"},
+    #                     headers={"Content-Type": "application/json"})
     # make_api_delete_call("http://127.0.0.1:5000/api/baby/1/Shalom")
+    make_api_get_call("http://127.0.0.1:5000/api/length_percentile_calculator/m/1/50")
+
